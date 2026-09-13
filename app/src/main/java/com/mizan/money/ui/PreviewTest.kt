6d01a90ff9c0dc77135b954402f031c0c5f3c0a4
Waleed <waleedhamadallah@gmail.com>
2026-09-13 22:36:16 +0300

    fix: remove @Preview annotation (missing tooling dependency)

--- app/src/main/java/com/mizan/money/ui/PreviewTest.kt
+++ app/src/main/java/com/mizan/money/ui/PreviewTest.kt
@@ -10,7 +10,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
@@ -200,7 +199,6 @@ private fun TintTile(label: String, elevation: androidx.compose.ui.unit.Dp) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 400, heightDp = 1500)
 @Composable
 private fun PreviewTestPreview() {
     PreviewTestScreen()
